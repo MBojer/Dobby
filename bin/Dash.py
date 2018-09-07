b0b0ebb6833c1a897505d0ab3f899294ceb0feab
Bojer <morten.bojer@accenture.com>
2018-09-08 00:45:05 +0100

    dash setup for monitorting agent

--- bin/Dash.py
+++ bin/Dash.py
@@ -13,98 +13,59 @@ import MySQLdb
 import pandas as pd
 
 
+db_Connection = MySQLdb.connect(host="192.168.1.2", user="dobby", passwd="HereToServe", db="DobbyLog")
+
+MonitorAgent_Agent_List = pd.read_sql("SELECT DISTINCT Agent FROM DobbyLog.MonitorAgent;", con=db_Connection)
+
+
 app = dash.Dash()
 
 app.layout = html.Div([
     html.H1('Dobby'),
 
-    html.H2('DC Voltage'),
-    html.Button('Update', id='DC_Voltage_Update_Button'),
-    dcc.Slider(
-        id='DC_Voltage_Slider',
-        min=10,
-        max=25000,
-        step=10,
-        value=100,
-    ),
-    dcc.Graph(id='DC_Voltage_Graph'),
 
-    html.H2('Humidity & Temperature'),
-    html.Button('Update', id='DHT_Update_Button'),
-    dcc.Slider(
-        id='DHT_Slider',
-        min=10,
-        max=25000,
-        step=10,
-        value=100,
-    ),
-    dcc.Graph(id='DHT_Graph'),
+    html.H2('MonitorAgent'),
 
-    html.H2('Bilge Level'),
-    html.Button('Update', id='Bilge_Update_Button'),
+    html.Button('Update', id='MonitorAgent_Update_Button'),
+
+    dcc.Dropdown(
+        id='MonitorAgent_Dropdown',
+        options=[{'label': Agents, 'value': Agents} for Agents in MonitorAgent_Agent_List.Agent],
+        ),
+
     dcc.Slider(
-        id='Bilge_Slider',
+        id='MonitorAgent_Slider',
         min=10,
         max=25000,
         step=10,
-        value=100,
-    ),
-    dcc.Graph(id='Bilge_Graph')
+        value=50,
+        ),
+
+    dcc.Graph(id='MonitorAgent_Graph'),
+
 ])
 
 
-@app.callback(Output('DC_Voltage_Graph', 'figure'), inputs=[Input('DC_Voltage_Slider', 'value'), Input('DC_Voltage_Update_Button', 'n_clicks')])
-def DC_Voltmeter_Update_Graph(Slider_Value, Number_Of_Clicks):
+@app.callback(Output('MonitorAgent_Graph', 'figure'), inputs=[Input('MonitorAgent_Slider', 'value'), Input('MonitorAgent_Dropdown', 'value'), Input('MonitorAgent_Update_Button', 'n_clicks')])
+def DC_Voltmeter_Update_Graph(Selected_Slider_Value, Selected_Dropdown, n_clicks):
 
-    db_Connection = MySQLdb.connect(host="192.168.1.2", user="dobby", passwd="HereToServe", db="DobbyLog")
-    df = pd.read_sql("SELECT DateTime, Value FROM DobbyLog.MonitorAgent WHERE Agent = 'DCVoltmeterWH' ORDER BY id DESC LIMIT " + str(Slider_Value) + ";", con=db_Connection)
+    if Selected_Dropdown is None:
+        return
 
-    return {
-        'data': [{
-            'x': df.DateTime,
-            'y': df.Value, 'name': "Wheelhouse",
-            'line': {
-                "shape": 'spline'}
-            }]}
+    db_Connection2 = MySQLdb.connect(host="192.168.1.2", user="dobby", passwd="HereToServe", db="DobbyLog")
 
+    df = pd.read_sql("SELECT DateTime, Source, Value FROM DobbyLog.MonitorAgent WHERE Agent = '" + str(Selected_Dropdown) + "' ORDER BY id DESC LIMIT " + str(Selected_Slider_Value) + ";", con=db_Connection2)
 
-@app.callback(Output('DHT_Graph', 'figure'), inputs=[Input('DHT_Slider', 'value'), Input('DHT_Update_Button', 'n_clicks')])
-def DHT_Update_Graph(Slider_Value, Number_Of_Clicks):
+    Return_Data = {'data': [{}]}
 
-    db_Connection = MySQLdb.connect(host="192.168.1.2", user="dobby", passwd="HereToServe", db="DobbyLog")
+    for i in df.Source.unique():
+            Return_Data["data"].append({
+                'x': df.DateTime[df['Source'] == i],
+                'y': df.Value[df['Source'] == i], 'name': i,
+                'line': {"shape": 'spline'}
+            })
 
-    df_Humidity = pd.read_sql("SELECT DateTime, Value FROM DobbyLog.MonitorAgent WHERE Source = '/1B/DHT/SternBilge/1/Humidity' ORDER BY id DESC LIMIT " + str(Slider_Value) + ";", con=db_Connection)
-    df_Temperature = pd.read_sql("SELECT DateTime, Value FROM DobbyLog.MonitorAgent WHERE Source = '/1B/DHT/SternBilge/1/Temperature' ORDER BY id DESC LIMIT " + str(Slider_Value) + ";", con=db_Connection)
-
-    return {
-        'data': [{
-            'x': df_Humidity.DateTime,
-            'y': df_Humidity.Value, 'name': "Humidity",
-            'line': {
-                "shape": 'spline'}
-            },
-            {
-            'x': df_Temperature.DateTime,
-            'y': df_Temperature.Value, 'name': "Temperature",
-            'line': {
-                "shape": 'spline',
-                'text': 'test'}
-            }]}
-
-
-@app.callback(Output('Bilge_Graph', 'figure'), inputs=[Input('Bilge_Slider', 'value'), Input('Bilge_Update_Button', 'n_clicks')])
-def Bilge_Update_Graph(Slider_Value, Number_Of_Clicks):
-
-    db_Connection = MySQLdb.connect(host="192.168.1.2", user="dobby", passwd="HereToServe", db="DobbyLog")
-    df = pd.read_sql("SELECT DateTime, Value FROM DobbyLog.MonitorAgent WHERE Agent = 'SternBilge' AND Value > 25 ORDER BY id DESC LIMIT " + str(Slider_Value) + ";", con=db_Connection)
-
-    return {
-        'data': [{
-            'x': df.DateTime,
-            'y': df.Value, 'name': "Stern Bilge",
-            'line': {
-                "shape": 'spline'}
-            }]}
+    return {'data': Return_Data}
 
 
 if __name__ == '__main__':
