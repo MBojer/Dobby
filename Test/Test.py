1de2f96af7a880be5a0d00d1b016baaad62885c0
Bojer <morten.bojer@accenture.com>
2018-10-26 00:00:29 +0100

    git test for update button

--- Test/Test.py
+++ Test/Test.py
@@ -6,3 +6,9 @@ if call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, 'w')) != 0:
     print("Nope!")
 else:
     print("Yup!")
+
+
+
+#     git rev-parse HEAD
+# 9f9d50837cec76849330fca56ab0abcd75d07045
+# 98765b85f438403a3966e70dcc302caf4cdbba22
